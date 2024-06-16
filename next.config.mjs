303cc5e0e18581160e0e74e79e6df31f5dcf9cbc
eugerr <gardensensei4.com>
2024-06-16 17:09:55 +0800

    added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,27 @@
-/** @type {import('next').NextConfig} */
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from 'next/constants.js'
+
 const nextConfig = {
   images: {
-    domains: ['s4.anilist.co', 'media.kitsu.io'],
+    remotePatterns: [
+      { hostname: 's4.anilist.co' },
+      {
+        hostname: 'media.kitsu.io',
+      },
+    ],
   },
 }
 
-export default nextConfig
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import('@ducanh2912/next-pwa')).default({
+      dest: 'public',
+    })
+    return withPWA(nextConfig)
+  }
+  return nextConfig
+}
+
+export default nextConfigFunction
